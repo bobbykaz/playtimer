fefd988f05e96ad784c6ff35265713552c313152
Bobby Kazimiroff <robert.kazimiroff@gmail.com>
2022-05-12 23:01:29 -0700

    Fix bug in laps, add beeps!

--- source/stopwatch.lua
+++ source/stopwatch.lua
@@ -18,6 +18,7 @@ local elapsedMs = 0.0
 local deltaMs = 0.0
 local laps = {}
 local newLap = false
+local tempLapMs = 0
 local newLapS,newLapMs = 0,0
 local stopS,stopMs = 0,0
 
@@ -43,8 +44,10 @@ local function ResetTimer()
 end
 
 local function Lap()
-    newLapS,newLapMs = playdate.getSecondsSinceEpoch()
-    newLap = true
+    if #laps < 10 then
+        newLapS,newLapMs = playdate.getSecondsSinceEpoch()
+        newLap = true
+    end
 end
 
 local function GetTimeString(durationMs)
@@ -85,7 +88,9 @@ local helpDrawX, helpDrawY = 60,205
 local function DrawHelp()
     if watchState == kState.running then
         fntBasic:drawTextAligned("Ⓑ STOP", helpDrawX, helpDrawY, kTextAlignment.left)
-        fntBasic:drawTextAligned("Ⓐ LAP", 400 - helpDrawX, helpDrawY, kTextAlignment.right)
+        if #laps <10 then
+            fntBasic:drawTextAligned("Ⓐ LAP", 400 - helpDrawX, helpDrawY, kTextAlignment.right)
+        end
     else 
         fntBasic:drawTextAligned("Ⓑ RESET", helpDrawX, helpDrawY, kTextAlignment.left)
         fntBasic:drawTextAligned("Ⓐ START", 400 - helpDrawX, helpDrawY, kTextAlignment.right)
@@ -93,13 +98,19 @@ local function DrawHelp()
 end
 
 local lapHeadingX,lapTimeX,lapDrawY = 20,100,100
+local lapCol2XOffset = 200
 local lapRowWidth = fntBasic:getHeight() + 1
 local function DrawLaps()
     local i = 1
     for i=1,#laps,1 
     do
-        fntBasic:drawTextAligned("Lap "..i..":", lapHeadingX, lapDrawY + (i-1)*lapRowWidth, kTextAlignment.left)
-        fntBasic:drawTextAligned(laps[i], lapTimeX, lapDrawY + (i-1)*lapRowWidth, kTextAlignment.left)
+        if i <= 5 then
+            fntBasic:drawTextAligned("Lap "..i..":", lapHeadingX, lapDrawY + (i-1)*lapRowWidth, kTextAlignment.left)
+            fntBasic:drawTextAligned(laps[i], lapTimeX, lapDrawY + (i-1)*lapRowWidth, kTextAlignment.left)
+        else
+            fntBasic:drawTextAligned("Lap "..i..":", lapCol2XOffset + lapHeadingX, lapDrawY + (i-6)*lapRowWidth, kTextAlignment.left)
+            fntBasic:drawTextAligned(laps[i], lapCol2XOffset + lapTimeX, lapDrawY + (i-6)*lapRowWidth, kTextAlignment.left)
+        end
     end
 end
 
@@ -123,13 +134,14 @@ end
 
 local function Update()
     if newLap then
-        local lapDelta = (newLapS - timeStartS) * 1000 + (newLapMs - timeStartMs)
+        local lapDelta = (newLapS - timeStartS) * 1000 + (newLapMs - timeStartMs) + tempLapMs
         local lapTime = GetTimeString(lapDelta)
         Log("lap",lapDelta,lapTime)
         table.insert(laps, lapTime)
         elapsedMs += lapDelta
         timeStartS,timeStartMs = newLapS,newLapMs
         deltaMs = 0
+        tempLapMs = 0
         newLap = false
     end
 
@@ -141,6 +153,7 @@ local function Update()
         deltaMs = (stopS - timeStartS) * 1000 + (stopMs - timeStartMs)
         Log("stopping",timeStartS,timeStartMs, deltaMs)
         elapsedMs += deltaMs
+        tempLapMs += deltaMs
         deltaMs = 0
         watchState = kState.stopped
         Log("timer stopped", elapsedMs)
@@ -148,12 +161,43 @@ local function Update()
     Draw()
 end
 
+local beepInst = nil
+local beepSeq = nil
+local function InitSounds()
+    local beepSnd = playdate.sound.synth.new(playdate.sound.kWaveSawtooth)
+    beepSnd:setADSR(0,1,1,0)
+    beepSeq = playdate.sound.sequence.new()
+    local track = playdate.sound.track.new()
+    beepInst = playdate.sound.instrument.new(beepSnd)
+    beepInst:setVolume(1.0)
+    track:addNote(1, "B7", 1)
+    track:addNote(2, "F7", 1)
+    track:setInstrument(beepInst)
+    beepSeq:setTempo(10)
+    beepSeq:addTrack(track)
+    --beepSeq:setLoops(1, 2, 1)
+end
+
+local function PlayStartBeep()
+    beepInst:playNote(3951, 1.0,0.1)
+end
+
+local function PlayStopBeep()
+    beepInst:playNote(2793, 1.0,0.1)
+end
+
+local function PlayResetBeep()
+    beepSeq:play()
+end
+
 local function HandleAButton()
+    
     if watchState == kState.running then
         Log("A button - Lapping")
         Lap()
     else
         Log("A button - starting")
+        PlayStartBeep()
         StartTimer()
     end
 end
@@ -161,9 +205,11 @@ end
 local function HandleBButton()
     if watchState == kState.running then
         Log("B button - stopping")
+        PlayStopBeep()
         StopTimer()
     else
         Log("B button - resetting")
+        PlayResetBeep()
         ResetTimer()
     end
 end
@@ -175,6 +221,7 @@ local function HandleCrank(change, accelChange)
 end
 
 function StopwatchScreenBuilder()
+    InitSounds()
     local screen = {
         AButton = HandleAButton,
         BButton = HandleBButton,
